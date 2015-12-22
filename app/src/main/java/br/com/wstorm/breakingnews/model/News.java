2b7380c48bd639fa2900579c16c7a3b6f2a55240
WBlackstorm <wevertonperon@gmail.com>
2015-12-22 13:23:24 -0200

    Commit inicial com a lista sendo carregada e levando para a tela de detalhes

---
+++ app/src/main/java/br/com/wstorm/breakingnews/model/News.java
@@ -0,0 +1,89 @@
+package br.com.wstorm.breakingnews.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by wstorm on 12/22/15.
+ */
+public class News implements Serializable {
+
+    private int id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private String category;
+
+    private String pubDate;
+
+    private String image;
+
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
